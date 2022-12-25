8fb7b8ee6ccfc33fb7f9aa561f0b865b0fed5ff5
syasul <githubku4@gmail.com>
2022-12-25 17:09:36 +0700

    add

--- MergeSort.java
+++ MergeSort.java
@@ -1,63 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package merge.sort;
 
-/**
- *
- * @author User
- */
+import java.util.Arrays;
+
 public class MergeSort {
-    
-    public static void initializemergeSort(int n[], int l, int h){ 
-        int rendah = l;
-        int tinggi = h;
-        if (rendah>=tinggi){
-           return;
+    // Merge two subarrays L and M into arr
+    void merge(int arr[], int p, int q, int r) {
+        // Create L ← A[p..q] and M ← A[q+1..r]
+        int n1 = q - p + 1;
+        int n2 = r - q;
+        int L[] = new int[n1];
+        int M[] = new int[n2];
+        for (int i = 0; i < n1; i++){
+            L[i] = arr[p + i];
         }
-        int tengah=(rendah+tinggi)/2;
-        initializemergeSort(n,rendah,tengah);
-        initializemergeSort(n,tengah+1,tinggi);
-        int end_low=tengah;
-        int awal_tinggi=tengah+1;
-        while ((l<=end_low) && (awal_tinggi<=tinggi)){
-           if (n[rendah]<n[awal_tinggi]){
-              rendah++;
-           }
-           else{
-              int Temp=n[awal_tinggi];
-              for (int k=awal_tinggi-1;k>=rendah;k--)
-              {
-                 n[k+1]=n[k];
-              }
-              n[rendah]=Temp;
-              rendah++;
-              end_low++;
-              awal_tinggi++;
-           }
+        for (int j = 0; j < n2; j++){
+            M[j] = arr[q + 1 + j];
+        }
+        // Maintain current index of sub-arrays and main array
+        int i, j, k;
+        i = 0;
+        j = 0;
+        k = p;
+        // Until we reach either end of either L or M, pick larger among
+        // elements L and M and place them in the correct position at A[p..r]
+        while (i < n1 && j < n2) {
+            if (L[i] <= M[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = M[j];
+                j++;
+            }
+            k++;
+        }
+        // When we run out of elements in either L or M,
+        // pick up the remaining elements and put in A[p..r]
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = M[j];
+            j++;
+            k++;
         }
     }
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Merge Sorting");
-        System.out.println();
- 
-        //Nomor yang akan di urutkan
-        int n[] = {20,12,7,11,14,5,19,3};
-        System.out.print("Sebelum di Urutkan = ");
-        for(int i = 0; i < n.length; i++){
-           System.out.print(n[i]+" ");
+    // Divide the array into two subarrays, sort them and merge them
+    void mergeSort(int arr[], int l, int r) {
+        if (l < r) {
+            // m is the point where the array is divided into two subarrays
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+
+
+            mergeSort(arr, m + 1, r);
+            // Merge the sorted subarrays
+            merge(arr, l, m, r);
         }
-        System.out.println();
-        initializemergeSort(n,0,n.length-1);
-        System.out.println();   
-        System.out.print("Setelah di Urutkan = ");
-        for(int i = 0; i < n.length; i++){
-           System.out.print(n[i]+" ");
-        }
+    }
+    // Print the array
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+        System.out.print(arr[i] + " ");
+
+    }
+    // Driver program
+    public static void main(String args[]) {
+        int arr[] = { 9,14,3,2,43,11,58,22};
+        System.out.println("Array sesudah disorting");
+        System.out.print(Arrays.toString(arr));
         System.out.println("");
+        MergeSort ob = new MergeSort();
+        ob.mergeSort(arr, 0, arr.length - 1);
+        System.out.println("Array sesudah disorting");
+        printArray(arr);
     }
 }
+
