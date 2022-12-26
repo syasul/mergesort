80cef4360e649259a39592f8487cca0631d2de07
syasul <githubku4@gmail.com>
2022-12-26 17:50:03 +0700

    add

--- MergeSort.java
+++ MergeSort.java
@@ -4,9 +4,9 @@ package merge.sort;
 import java.util.Arrays;
 
 public class MergeSort {
-    // Merge two subarrays L and M into arr
+    // menggabungkan dua sub-array L dan M menjadi arr
     void merge(int arr[], int p, int q, int r) {
-        // Create L ← A[p..q] and M ← A[q+1..r]
+        // Buat L ← A[p..q] dan M ← A[q+1..r]
         int n1 = q - p + 1;
         int n2 = r - q;
         int L[] = new int[n1];
@@ -17,13 +17,13 @@ public class MergeSort {
         for (int j = 0; j < n2; j++){
             M[j] = arr[q + 1 + j];
         }
-        // Maintain current index of sub-arrays and main array
+        // Pertahankan indeks sub-array dan larik utama saat ini
         int i, j, k;
         i = 0;
         j = 0;
         k = p;
-        // Until we reach either end of either L or M, pick larger among
-        // elements L and M and place them in the correct position at A[p..r]
+        // Sampai kita mencapai salah satu ujung L atau M, pilih yang lebih besar
+         // elemen L dan M dan letakkan di posisi yang benar di A[p..r]
         while (i < n1 && j < n2) {
             if (L[i] <= M[j]) {
                 arr[k] = L[i];
@@ -34,8 +34,8 @@ public class MergeSort {
             }
             k++;
         }
-        // When we run out of elements in either L or M,
-        // pick up the remaining elements and put in A[p..r]
+        // Saat kita kehabisan elemen di L atau M,
+         // ambil elemen yang tersisa dan masukkan A[p..r]
         while (i < n1) {
             arr[k] = L[i];
             i++;
@@ -47,30 +47,30 @@ public class MergeSort {
             k++;
         }
     }
-    // Divide the array into two subarrays, sort them and merge them
+    // Bagilah array menjadi dua subarray, urutkan dan gabungkan
     void mergeSort(int arr[], int l, int r) {
         if (l < r) {
-            // m is the point where the array is divided into two subarrays
+            // m adalah titik di mana array dibagi menjadi dua subarray
             int m = (l + r) / 2;
             mergeSort(arr, l, m);
 
 
             mergeSort(arr, m + 1, r);
-            // Merge the sorted subarrays
+            //Menggabungkan subarray yang telah diurutkan
             merge(arr, l, m, r);
         }
     }
-    // Print the array
+    // Cetak larik
     static void printArray(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; ++i)
         System.out.print(arr[i] + " ");
 
     }
-    // Driver program
+    // Program pengemudi
     public static void main(String args[]) {
-        int arr[] = { 9,14,3,2,43,11,58,22};
-        System.out.println("Array sesudah disorting");
+        int arr[] = { 9};
+        System.out.println("Array sebelum disorting");
         System.out.print(Arrays.toString(arr));
         System.out.println("");
         MergeSort ob = new MergeSort();
